cfc33bccb915c6c511c7e28833ca1add69e6fb4f
nicocarnebia <nicocarnebia@gmail.com>
2015-05-14 00:56:21 -0300

    Added current database sql file in "/db/Youtuve.sql"

---
+++ db/youtuve.sql
@@ -0,0 +1,292 @@
+-- phpMyAdmin SQL Dump
+-- version 4.3.11
+-- http://www.phpmyadmin.net
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 14-05-2015 a las 05:54:09
+-- Versión del servidor: 5.6.24
+-- Versión de PHP: 5.6.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de datos: `youtuve`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `channel`
+--
+
+CREATE TABLE IF NOT EXISTS `channel` (
+  `id` int(11) NOT NULL,
+  `idUser` int(11) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `description` varchar(500) NOT NULL,
+  `frontImgUrl` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `comment`
+--
+
+CREATE TABLE IF NOT EXISTS `comment` (
+  `id` int(11) NOT NULL,
+  `idUser` int(11) NOT NULL,
+  `idVideo` int(11) NOT NULL,
+  `comment` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `follower`
+--
+
+CREATE TABLE IF NOT EXISTS `follower` (
+  `idUser` int(11) NOT NULL,
+  `IdUserToFollow` int(11) NOT NULL,
+  `seen` tinyint(4) NOT NULL DEFAULT '0',
+  `confirmed` tinyint(4) NOT NULL DEFAULT '0',
+  `date` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `playlist`
+--
+
+CREATE TABLE IF NOT EXISTS `playlist` (
+  `id` int(11) NOT NULL,
+  `Name` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `rate`
+--
+
+CREATE TABLE IF NOT EXISTS `rate` (
+  `idUser` int(11) NOT NULL,
+  `idVideo` int(11) NOT NULL,
+  `rate` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `suggestion`
+--
+
+CREATE TABLE IF NOT EXISTS `suggestion` (
+  `idUser` int(11) NOT NULL,
+  `idUserToSuggest` int(11) NOT NULL,
+  `idVideo` int(11) NOT NULL,
+  `seen` tinyint(4) NOT NULL DEFAULT '0',
+  `date` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tag`
+--
+
+CREATE TABLE IF NOT EXISTS `tag` (
+  `id` int(11) NOT NULL,
+  `name` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `user`
+--
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(30) NOT NULL,
+  `name` int(30) NOT NULL,
+  `lastname` int(30) NOT NULL,
+  `birthday` date NOT NULL,
+  `gender` char(1) NOT NULL,
+  `thumbUrl` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `video`
+--
+
+CREATE TABLE IF NOT EXISTS `video` (
+  `id` int(11) NOT NULL,
+  `idUser` int(11) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `link` varchar(30) NOT NULL,
+  `date` date NOT NULL,
+  `durationInSeconds` int(11) NOT NULL,
+  `active` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `videoplaylist`
+--
+
+CREATE TABLE IF NOT EXISTS `videoplaylist` (
+  `idVideo` int(11) NOT NULL,
+  `idPlaylist` int(11) NOT NULL,
+  `isWatchLater` tinyint(4) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `videotag`
+--
+
+CREATE TABLE IF NOT EXISTS `videotag` (
+  `idVideo` int(11) NOT NULL,
+  `idTag` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `viewhistory`
+--
+
+CREATE TABLE IF NOT EXISTS `viewhistory` (
+  `idUser` int(11) NOT NULL,
+  `idVideo` int(11) NOT NULL,
+  `date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `channel`
+--
+ALTER TABLE `channel`
+  ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `idUser` (`idUser`);
+
+--
+-- Indices de la tabla `comment`
+--
+ALTER TABLE `comment`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `follower`
+--
+ALTER TABLE `follower`
+  ADD PRIMARY KEY (`idUser`,`IdUserToFollow`);
+
+--
+-- Indices de la tabla `playlist`
+--
+ALTER TABLE `playlist`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `rate`
+--
+ALTER TABLE `rate`
+  ADD PRIMARY KEY (`idUser`,`idVideo`);
+
+--
+-- Indices de la tabla `suggestion`
+--
+ALTER TABLE `suggestion`
+  ADD PRIMARY KEY (`idUser`,`idUserToSuggest`,`idVideo`);
+
+--
+-- Indices de la tabla `tag`
+--
+ALTER TABLE `tag`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Indices de la tabla `video`
+--
+ALTER TABLE `video`
+  ADD PRIMARY KEY (`id`), ADD KEY `FK_user` (`idUser`);
+
+--
+-- Indices de la tabla `videoplaylist`
+--
+ALTER TABLE `videoplaylist`
+  ADD PRIMARY KEY (`idVideo`,`idPlaylist`);
+
+--
+-- Indices de la tabla `videotag`
+--
+ALTER TABLE `videotag`
+  ADD PRIMARY KEY (`idVideo`,`idTag`);
+
+--
+-- Indices de la tabla `viewhistory`
+--
+ALTER TABLE `viewhistory`
+  ADD PRIMARY KEY (`idUser`,`idVideo`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `channel`
+--
+ALTER TABLE `channel`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `comment`
+--
+ALTER TABLE `comment`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `playlist`
+--
+ALTER TABLE `playlist`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `tag`
+--
+ALTER TABLE `tag`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `video`
+--
+ALTER TABLE `video`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
