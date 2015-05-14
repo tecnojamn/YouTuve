0fab4d58a92b1bccad73671e9c9b3ed13b3d4e7c
nicocarnebia <nicocarnebia@gmail.com>
2015-05-14 01:40:16 -0300

    Login Form added

--- application/views/homePage.php
+++ application/views/homePage.php
@@ -5,10 +5,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <head>
         <meta charset="utf-8">
         <title>YouTuve</title>
+        <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
     </head>
 
     <body>
-        <h3>This is Youtuve</h3>
+        <?php $this->view('header_default');//se agrega el header ala estructura ?> 
         <div style="position: fixed;bottom: 0;background: #f8f8f8;height:40px; width:100%;"></div>
     </body>
 </html>
