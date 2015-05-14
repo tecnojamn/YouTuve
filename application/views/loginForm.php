67a8e4dbf8ab772db8de768433f8c3367da1813e
nicocarnebia <nicocarnebia@gmail.com>
2015-05-14 01:44:50 -0300

    loginForm.php added

---
+++ application/views/loginForm.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Youtuve-Login</title>
+        <link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css">
+    </head>
+    <body>
+        <form action="User/login" method="post">
+            <input type="text" placeholder="Username" name="uname"/>
+            <input type="password" placeholder="Password" name="upass"/>
+            <input type="submit" value="Enter" name="whocares"/>
+        </form>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
