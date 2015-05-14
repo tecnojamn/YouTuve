2fd0c52584eba06ceb008847dac7be8fcd7c6306
tecnologo <tecnologo@tecnologo.(none)>
2015-05-14 09:04:39 -0300

    home prueba

--- application/views/homePage.php
+++ application/views/homePage.php
@@ -10,6 +10,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
     <body>
         <?php $this->view('header_default');//se agrega el header ala estructura ?> 
+        <p>hola mundou</p>
         <div style="position: fixed;bottom: 0;background: #f8f8f8;height:40px; width:100%;"></div>
     </body>
 </html>
