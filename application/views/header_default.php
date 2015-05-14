f9881211e5932abb1e693eb15d50da83e7edf8aa
tecnologo <tecnologo@tecnologo.(none)>
2015-05-14 12:04:30 -0300

    Mer added, chandes in header css

--- application/views/header_default.php
+++ application/views/header_default.php
@@ -9,7 +9,7 @@
         <nav class="navbar navbar-default">
             <div class="container-fluid">
 
-                <div class="navbar-header">
+                <div class="navbar-header">                    
                     <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="true">
                         <span class="sr-only">Toggle navigation</span>
                         <span class="icon-bar"></span>
@@ -17,18 +17,17 @@
                         <span class="icon-bar"></span>
                     </button>
                     <a class="navbar-brand" href="#">YouTuve</a>
-
-
                 </div>
 
                 <div class="navbar-collapse  collapse in" id="bs-example-navbar-collapse-1" aria-expanded="true">
-                    <form class="navbar-form navbar-left" role="search">
-                        <div class="form-group">
-                            
-                            <input type="text" class="form-control" placeholder="Search">
-                        </div>
-                        <button type="submit" class="btn btn-default">Search</button>
-                    </form>
+                    <div class="col-md-3">
+                        <form class="navbar-form navbar-left" role="search">
+                            <div class="form-group">
+                                <input type="text" class="form-control" placeholder="Search">
+                            </div>
+                            <button type="submit" class="btn btn-default">Search</button>
+                        </form>
+                    </div>
                     <ul class="nav navbar-nav navbar-right">
                         <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
