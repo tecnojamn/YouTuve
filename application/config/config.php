ce21df92830547d7b4bf6508e475209ef66ebb58
Andrés Peña <apena@cualit.com>
2015-09-06 16:04:03 -0300

    fix ambiguous active field in video model

--- application/config/config.php
+++ application/config/config.php
@@ -17,7 +17,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = 'http://'.$_SERVER['SERVER_NAME'].':8080/YouTuve';
+$config['base_url'] = 'http://'.$_SERVER['SERVER_NAME'].'/YouTuve';
 //$config['base_url'] = 'http://'.$_SERVER['SERVER_NAME'].':8080/YouTuve';
 
 //$config['index_page'] = '';
