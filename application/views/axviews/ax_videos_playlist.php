f6f1a75dff72d3255be841b6acf56095a3426e78
julito66 <julito.gg@gmail.com>
2015-09-09 21:40:46 -0300

    PlayList con autoplay Con diseño feito

--- application/views/axviews/ax_videos_playlist.php
+++ application/views/axviews/ax_videos_playlist.php
@@ -1,18 +1,18 @@
 <?php foreach ($playlist->videos->list as $row) { ?>
-    <div class="col-lg-12 well-violet" style="background-color: white;
+    <div class="videoInfo col-lg-4 well-violet" style="background-color: white;
          padding: 15px;margin-top: 15px">
+        <input id="id" type="hidden" value="<?php echo $row->id ?>">
+        <input id="link" type="hidden" value="<?php echo $row->link ?>">
         <div class="col-lg-2" style="width: 138px">
-            <a href="<?php echo base_url(); ?>video/view/<?php echo $row->id ?>">
+            <a href="#" class="changeVideoImg">
                 <img src="http://img.youtube.com/vi/<?php echo $row->link ?>/0.jpg" style="width: 100px;height: 100px;float: left;">
             </a>
         </div>
-        <div class="col-lg-9">
-            <a href="<?php echo base_url(); ?>video/view/<?php echo $row->id ?>"><?php echo $row->name ?></a><br>
-
+        <div class="col-lg-2">
+            <a href="#" class="changeVideo"><?php echo $row->name ?></a>
         </div>
         <div class="col-lg-1" style="float: right">
-            <a href="#" class="delVideo btn btn-danger" style="float: right">X</a>
-            <input type="hidden" value="<?php echo $row->id?>">
+            <a href="#" class="delVideo btn btn-danger" style="float: right">X</a>  
         </div>
     </div>
 <?php } ?>
--- application/views/prueba.php
+++ application/views/prueba.php
@@ -14,16 +14,45 @@ $this->load->helper('url');
     </head>
 
     <body>
+
         <?php (isset($log) && $log) ? $this->load->view('header') : $this->load->view('header_default'); ?>
+        <div id="player"></div>
+        <script type="text/javascript" id="www-widgetapi-script" src="https://s.ytimg.com/yts/jsbin/www-widgetapi-vfl3I-GuN/www-widgetapi.js" async=""></script>
+        <script src="https://www.youtube.com/iframe_api"></script>
+        <script>
 
-        <form action="<?php echo base_url(); ?>User/prueba" method="post">
+            // 3. This function creates an <iframe> (and YouTube player)
+            //    after the API code downloads.
+            var player;
+            function onYouTubeIframeAPIReady() {
+                player = new YT.Player('player', {
+                    height: '390',
+                    width: '640',
+                    videoId: 'M7lc1UVf-VE',
+                    events: {
+                        'onReady': onPlayerReady,
+                        'onStateChange': onPlayerStateChange
+                    }
+                });
+            }
 
-            <input type="text" placeholder="idUser" name="idUser"/>
-           
-            <input type="submit" value="Enter" name="whocares"/>
-        </form>
+            // 4. The API will call this function when the video player is ready.
+            function onPlayerReady(event) {
+                event.target.playVideo();
+            }
 
-
+            // 5. The API calls this function when the player's state changes.
+            //    The function indicates that when playing a video (state=1),
+            //    the player should play for six seconds and then stop.
+            function onPlayerStateChange(event) {
+                if (event.data == 0) {
+                    player.cueVideoById("u1zgFlCw8Aw", 10, "medium");
+                }
+            }
+            function stopVideo() {
+                player.stopVideo();
+            }
+        </script>
         <?php $this->load->view('footer'); ?>
     </body>
 </html>
