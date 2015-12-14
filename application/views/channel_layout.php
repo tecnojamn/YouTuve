ead116d9fbb308a9bbf29dc1a0d4a1b6090bd4e8
julito66 <julito.gg@gmail.com>
2015-12-13 22:08:34 -0300

    mas arreglitos

--- application/views/channel_layout.php
+++ application/views/channel_layout.php
@@ -17,16 +17,16 @@ $this->load->helper('url');
         <?php (isset($log) && $log) ? $this->load->view('header') : $this->load->view('header_default'); ?>
 
         <?php
-        if(isset($_GET["msg"])){ 
-            $success= $_GET["msg"] == "success"?  true: false;
+        if (isset($_GET["msg"])) {
+            $success = $_GET["msg"] == "success" ? true : false;
             ?> <div class="row" style="min-height:50px;">
                 <div class="col-lg-12">
-                    <div class="alert alert-dismissible <?php echo $success? "alert-success": "alert-danger"; ?> "><button type="button" class="close" data-dismiss="alert">×</button>
-           <?php   echo $success? "El video ha sido eliminado":"Error al eliminar el video"; ?>
+                    <div class="alert alert-dismissible <?php echo $success ? "alert-success" : "alert-danger"; ?> "><button type="button" class="close" data-dismiss="alert">×</button>
+                        <?php echo $success ? "El video ha sido eliminado" : "Error al eliminar el video"; ?>
                     </div>
                 </div>
             </div>
-        <?php
+            <?php
         }
         ?>
         <?php
@@ -39,12 +39,12 @@ $this->load->helper('url');
             <?php
         }
         ?>
-        <?php if ($channel !== null && $channel->videos !== null) {
+        <?php if ($channel !== null) {
             ?>
-        
+
             <div class="col-lg-12">
                 <div style="  background-image: url(<?php echo base_url() . ALT_CHANNEL_BACKGROUND_PIC; ?>);color: #f8f8f8;width: 100%;height: auto;overflow: hidden;padding: 15px 5px;">
-                    <div class="col-lg-1"> <img style="width:100%;border: 1px solid rgb(213, 213, 213);" src="<?php echo base_url() . ALT_PLAYLIST_PIC//$channel->frontImgUrl          ?>"/></div>
+                    <div class="col-lg-1"> <img style="width:100%;border: 1px solid rgb(213, 213, 213);" src="<?php echo base_url() . ALT_PLAYLIST_PIC//$channel->frontImgUrl           ?>"/></div>
                     <div class="col-lg-11">
                         <p style="  font-size: 20px;line-height: 55px;font-weight: bold;color: white;"><?php echo $channel->name ?></p>
                     </div>
@@ -53,10 +53,10 @@ $this->load->helper('url');
                     <p><?php echo $channel->description ?></p>
                 </div>
                 <div style="background-color: white;width: 100%;height: auto;overflow: hidden; border-top: 1px solid rgb(242, 242, 242); padding: 15px">
-                    <p><?php echo $channel->followersCount ==1 ?  $channel->followersCount." suscripción" : $channel->followersCount." suscripciones";  ?></p>
+                    <p><?php echo $channel->followersCount == 1 ? $channel->followersCount . " suscripción" : $channel->followersCount . " suscripciones"; ?></p>
                 </div>
                 <div style="background-color: white;width: 100%;height: auto;overflow: hidden; border-top: 1px solid rgb(242, 242, 242); padding: 15px">
-                    <?php $this->load->view('social_share_buttons.php');?>                 
+                    <?php $this->load->view('social_share_buttons.php'); ?>                 
                 </div>
                 <div style="  font-size: 20px;background-color:white; "class="col-lg-12">
                     <p>Videos:</p>
@@ -64,35 +64,39 @@ $this->load->helper('url');
             </div>
 
             <div class="col-lg-12" style="background-color: rgb(219, 219, 219);margin-bottom: 20px;padding-bottom: 15px;">
-                <?php foreach ($channel->videos->list as $v) { ?>
-                    <div class="col-lg-12 well-blue" style="background-color: white;
-                         padding: 15px;
-                         border-top: 1px solid rgb(242, 242, 242);">
-                        <div class="col-lg-2" style="width: 130px">
-                            <a href="<?php echo base_url(); ?>video/view/<?php echo $v->id ?>">
-                                <img src="http://img.youtube.com/vi/<?php echo $v->link ?>/0.jpg" style="width: 100px;height: 100px;float: left;">
-                            </a>
-                        </div>
-                        <div class="col-lg-10">
-                            <a href="<?php echo base_url(); ?>video/view/<?php echo $v->id ?>"><?php echo $v->name ?></a><br>
-                            <a href="<?php echo base_url(); ?>channel/view/<?php echo $channel->id ?>"><?php echo $channel->name ?></a><br>
-                            <p>
+                <?php if ($channel->videos != null) {
+                    foreach ($channel->videos->list as $v) {
+                        ?>
+                        <div class="col-lg-12 well-blue" style="background-color: white;
+                             padding: 15px;
+                             border-top: 1px solid rgb(242, 242, 242);">
+                            <div class="col-lg-2" style="width: 130px">
+                                <a href="<?php echo base_url(); ?>video/view/<?php echo $v->id ?>">
+                                    <img src="http://img.youtube.com/vi/<?php echo $v->link ?>/0.jpg" style="width: 100px;height: 100px;float: left;">
+                                </a>
+                            </div>
+                            <div class="col-lg-10">
+                                <a href="<?php echo base_url(); ?>video/view/<?php echo $v->id ?>"><?php echo $v->name ?></a><br>
+                                <a href="<?php echo base_url(); ?>channel/view/<?php echo $channel->id ?>"><?php echo $channel->name ?></a><br>
+                                <p>
                                 <?php echo $v->views ?> Visualizaciones
-                            </p>
-                            <?php
-                            if( $this->session->userdata("userId")==$channel->idUser){
-                                echo "<a href='".base_url()."video/remove/".$v->id."'>Eliminar video</a>";
-                                //echo "<a href='"+ base_url(); +"channel/view" + "'>"+ $channel->id +"</a><br>";                                
-                            }   
-                            ?>
+                                </p>
+                                <?php
+                                if ($this->session->userdata("userId") == $channel->idUser) {
+                                    echo "<a href='" . base_url() . "video/remove/" . $v->id . "'>Eliminar video</a>";
+                                    //echo "<a href='"+ base_url(); +"channel/view" + "'>"+ $channel->id +"</a><br>";                                
+                                }
+                                ?>
 
+                            </div>
                         </div>
-                    </div>
-                <?php } ?>
+                    <?php }
+                }
+                ?>
             </div>
         <?php } else { ?>
 
-        <?php }$this->load->view('footer'); ?>
+<?php }$this->load->view('footer'); ?>
     </body>
 </html>
 
